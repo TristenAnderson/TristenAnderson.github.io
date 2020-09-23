605a98cebe198cbded06d9e6a39cf298a2990a05
TristenAnderson <tristendanderson@gmail.com>
2020-09-22 19:01:08 -0500

    minor style changes to footer

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -7,27 +7,32 @@ function Footer() {
             minHeight: "50px",
             backgroundColor: "#0d2135",
             color: "#f5f5f5",
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            position: 'relative',
+            textAlign: "center",
+    }
+
+    const barStyle = {
+        display: "flex",
+        justifyContent: "center",
     }
 
     const linkStyle = {
         color: "#5ff0ce",
         textDecoration: "none",
+        display: "inline-block",
     }
 
     return(
-        <div style={useStyles}>
-            <a
-             href="https://github.com/TristenAnderson"
-             target="_blank"
-             rel="noopener noreferrer"
-             style={linkStyle}>
-                 Built and Designed by Tristen Anderson
-             </a>
-        </div>
+        <footer style={useStyles}>
+            <div style={barStyle}>
+                <a
+                href="https://github.com/TristenAnderson"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={linkStyle}>
+                    Built and Designed by Tristen Anderson
+                </a>
+             </div>
+        </footer>
     )
 
 }
